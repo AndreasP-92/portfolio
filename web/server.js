530dcd7e2e98b40cf43cd939cdfd83935c8af65c
andreasP-92 <4856456@rts-365.dk>
2018-02-22 02:44:20 +0100

    asd

--- web/server.js
+++ web/server.js
@@ -1,5 +1,5 @@
 const browserSync = require('browser-sync').create();
-browserSync.watch('./public_html/**/*').on('change', browserSync.reload);
+browserSync.watch('./public/**/*').on('change', browserSync.reload);
 browserSync.init({
-    'server': './public_html'
-});
+    'server': './public'
+});s
