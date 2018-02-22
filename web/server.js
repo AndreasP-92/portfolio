d9b4ff30549f07d145a5b3dc18eb5013610ba6e4
andreasP-92 <4856456@rts-365.dk>
2018-02-22 02:52:40 +0100

    server.js update

--- web/server.js
+++ web/server.js
@@ -1,5 +1,14 @@
-const browserSync = require('browser-sync').create();
-browserSync.watch('./public/**/*').on('change', browserSync.reload);
-browserSync.init({
-    'server': './public'
-});s
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, '/public_html')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
