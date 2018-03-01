0eaaab66499cee96333cf9d150b44b4d98e134b2
andreasP-92 <4856456@rts-365.dk>
2018-03-01 12:37:57 +0100

    alt fjernet

--- web/public_html/drop_down/hifi/web/public/js/helpers.js
+++
@@ -1,3 +0,0 @@
-const SiteAdress = window.location.protocol + '//' + window.location.hostname + ':3000'
-const ApiAdress = window.location.protocol + '//' + window.location.hostname + ':1337'
-const host = "http://138.197.186.159"
--- web/public_html/drop_down/hifi/web/public/js/loggedin.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", event => {
-    if (localStorage.getItem('token') === null) {
-          window.location.assign('login.html');
-    }
-});
