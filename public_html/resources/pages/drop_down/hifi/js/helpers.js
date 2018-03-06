538f619d9e60319715b0368b68189dffa6d02b27
andreasP-92 <4856456@rts-365.dk>
2018-03-06 12:01:23 +0100

    const helper update #2

--- public_html/resources/pages/drop_down/hifi/js/helpers.js
+++ public_html/resources/pages/drop_down/hifi/js/helpers.js
@@ -1,4 +1,4 @@
-const SiteAdress = window.location.protocol + '//' + window.location.hostname + ':1337'
-const ApiAdress = window.location.protocol + '//' + window.location.hostname + ':1337'
+const SiteAdress = window.location.protocol + '//' + window.location.hostname + ':80'
+const ApiAdress = window.location.protocol + '//' + window.location.hostname + ':80'
 const host = "http://138.197.186.159";
 // const host = "http://localhost";
