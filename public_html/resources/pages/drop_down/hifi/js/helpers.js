cd689c3b9626d9f4bf3fa9e53969997ab5218605
andreasP-92 <4856456@rts-365.dk>
2018-03-06 14:08:22 +0100

    update 0.3.3 #3

--- public_html/resources/pages/drop_down/hifi/js/helpers.js
+++ public_html/resources/pages/drop_down/hifi/js/helpers.js
@@ -1,6 +1,6 @@
 const SiteAdress = window.location.protocol + '//' + window.location.hostname + ':80'
 const ApiAdress = window.location.protocol + '//' + window.location.hostname + ':80'
-const host = "http://138.197.186.159";
+const host = "http://mrcaptain.info";
 // const host = "http://localhost";
-const port = 80;
+// const port = 80;
 // const port = 1337
