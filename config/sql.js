15fc5956db0914b37a74fb5ec8bdfa61895ce112
andreasP-92 <4856456@rts-365.dk>
2018-03-07 20:19:04 +0100

    responsive update 2.1

--- config/sql.js
+++ config/sql.js
@@ -1,29 +1,29 @@
 const mysql = require('mysql2');
 
-// module.exports = {
-//     'connect': () => {
-//         return mysql.createConnection({
-//             'host': '138.197.186.159',
-//             'user': 'root',
-//             'password': '4321',
-//             'database': 'hifi'
-//         });
-//         doMultipleQueries(function(err){
-//             connect.end();
-//         })
-//     }
-// };
+module.exports = {
+    'connect': () => {
+        return mysql.createConnection({
+            'host': '138.197.186.159',
+            'user': 'root',
+            'password': '4321',
+            'database': 'hifi'
+        });
+        doMultipleQueries(function(err){
+            connect.end();
+        })
+    }
+};
 
 
 // ********************************************************* TESTING ********************************************************
 
-module.exports = {
-    'connect': () => {
-        return mysql.createConnection({
-            'host': 'localhost',
-            'user': 'root',
-            'password': '',
-            'database': 'hifi'
-        });
-    }
-}; 
+// module.exports = {
+//     'connect': () => {
+//         return mysql.createConnection({
+//             'host': 'localhost',
+//             'user': 'root',
+//             'password': '',
+//             'database': 'hifi'
+//         });
+//     }
+// }; 
