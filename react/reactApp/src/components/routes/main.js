85358790caab930c5d55bc45efc6734dc4d7b9b3
AndreasP-92 <31003640+AndreasP-92@users.noreply.github.com>
2018-10-27 22:39:03 +0200

    Update react/reactApp/src/components/routes/main.js

--- react/reactApp/src/components/routes/main.js
+++ react/reactApp/src/components/routes/main.js
@@ -1,61 +1,19 @@
-import React from "react";
-import Main     from './components/routes/main'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import React                from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Index from './components/index/index'
-import WeatherApp  from './components/weatherApp/weatherApp'
-import CalculatorApp from './components/calculater/calculaterApp'
+// import Index from '../index/index'
+// import WeatherApp  from '../weatherApp/weatherApp'
+// import CalculaterApp from '../calculater/calculaterApp'
 
-class App extends React.Component{
 
-  render(){
-      return(
-      <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/weatherapp">Weather App</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculator</Link>
-          </li>
-        </ul>
+// const Main = () =>(
+//   <Router>
+//     <Switch>
+//       <Route exact path="/" component             ={Index} />
+//       <Route path="/weatherapp" component         ={WeatherApp} />
+//       <Route path="/calculator" component         ={CalculaterApp} />
+//     </Switch>
+//   </Router>
+// )
 
-        <hr />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/weatherapp" component={weatherAppRoute} />
-        <Route path="/calculator" component={calculatorAppRoute} />
-      </div>
-    </Router>
-    )
-  }
-  
-}
-function Home() {
-  return (
-    <div>
-      <Index/>
-    </div>
-  );
-}
-
-function weatherAppRoute() {
-  return (
-    <div>
-      <WeatherApp/>
-    </div>
-  );
-}
-function calculatorAppRoute() {
-  return (
-    <div>
-      <CalculatorApp/>
-    </div>
-  );
-}
-
-export default App;  
+// export default Main;
