8cec44488da5ca7fa89c2d98ee7d10dc9384e539
AndreasP-92 <andreas1992@outlook.dk>
2018-11-26 11:35:42 +0100

    update 1.1.1

--- routes/perfect-shoes_routes/config/database.js
+++ routes/perfect-shoes_routes/config/database.js
@@ -1,15 +1,32 @@
+// const mysql = require('mysql2')
+
+// module.exports = {
+//     'connect': () => {
+//         return mysql.createConnection({
+//             'host': 'localhost',
+//             'user': 'root',
+//             'password': '',
+//             'database': 'port_perfectshoes'
+//         });
+//         doMultipleQueries(function(err){
+//             connect.end();
+//        })
+//     },
+// }
+
 const mysql = require('mysql2')
 
 module.exports = {
     'connect': () => {
         return mysql.createConnection({
-            'host': 'localhost',
+            'host': '138.197.186.159',
             'user': 'root',
-            'password': '',
-            'database': 'port_perfectshoes'
+            'password': '1234',
+            'database': 'port_perfect_shoes'
         });
         doMultipleQueries(function(err){
             connect.end();
-       })
+        })
     },
 }
+
